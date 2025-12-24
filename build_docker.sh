22c08979392ae3f584959c99df8a9d293a5324b5
cangkui <aolangwu@qq.com>
2025-12-24 11:14:08 +0800

    fix: build by docker image.

--- build_docker.sh
+++ build_docker.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# build docker image for compiling first
 # docker build -t iec:4.08 .
 
+# run for only project build/compile
 # docker run --rm \
 #     -v "$(pwd)":/home/opam/src \
 #     -u opam \
+#     -w /home/opam/src \
 #     iec:4.08 \
-#     make build
+#     /bin/bash -c 'eval $(opam env) && make build'
 
+# create and run for server running.
 # docker create -it \
 #     -v "$(pwd)":/home/opam/src \
 #     -u opam \
+#     --name iec-serve \
 #     iec:4.08
+# docker start iec-serve
